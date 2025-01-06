3d58fc8243a4fe8d9f36a330be872c580f6c513f
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-01-06 22:18:51 +0100

    Create zad181.py

---
+++ zad181.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self,val,next=None):
+        self.val=val
+        self.next=next
+
+def delatelast(head):
+    cur=head
+    while cur.next.next:
+        cur=cur.next
+    cur.next=None
+    return head
+def creator(T):
+    head=Node(T[0])
+    cur=head
+    for i in range(1,len(T)):
+        cur.next=Node(T[i])
+        cur=cur.next
+    return head
+def show(head):
+    if head==None:
+        print()
+        return
+    print(head.val,end='->')
+    return show(head.next)
+
+T1=[1,3,7,4,5,4,2]     
+show(creator(T1))
+show(delatelast(creator(T1)))      
